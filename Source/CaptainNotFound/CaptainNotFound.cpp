c8d3b95be54c66c74b76225b3d7cb4f9352a6488
IcyButSpicy <dom.tokarz@gmail.com>
2025-07-28 15:17:26 +0200

    Init
    Init push for 7700: Captain Not Found! game

---
+++ Source/CaptainNotFound/CaptainNotFound.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CaptainNotFound.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CaptainNotFound, "CaptainNotFound" );
